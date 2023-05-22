f30878868f246539ab531555725b06cb09aa895b
decourtney <donovan.courtney@gmail.com>
2023-05-22 09:54:16 -0700

    projects display work

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { motion, stagger, useAnimation, useAnimate } from "framer-motion";
+import githubLogo from "../../images/github-mark-white.svg"
 
+interface Project {
+  name: string;
+  date?: string;
+  description: string[];
+  tags?: string[];
+  url?: string;
+  repo: string;
+  image?: string;
+}
 
 const detailsVariants = {
   open: {
@@ -13,13 +23,11 @@ const detailsVariants = {
   },
 };
 
-const ProjectDetails = () => {
-  // const [isDetails, setIsDetails] = useState(false);
-
+const ProjectDetails = ({ data }: { data: Project }) => {
+  console.log(data)
   return (
     <>
-      {/* <div className="absolute w-screen h-screen top-1/2 left-0 -translate-y-[50%] -translate-x-[50%] backdrop-blur-[2px]" /> */}
-      <div className="absolute top-1/2 left-0 w-fit h-fit max-h-screen transform -translate-y-[50%] -translate-x-[50%]">
+      <div className="absolute top-1/2 left-0 w-fit h-fit max-w-fit max-h-screen transform -translate-y-[50%] -translate-x-[50%]">
         <motion.div
           className="relative"
           initial="close"
@@ -29,28 +37,27 @@ const ProjectDetails = () => {
           layout
         >
           <div className="details-border absolute w-full h-full z-10 pointer-events-none" />
-          <div className="details-lighting flex-col w-full max-h-screen p-[5%] text-[#ffffff] overflow-auto scrollbar-hide">
-            <h1 className="text-center text-7xl pb-[2%]">Lorem ipsum</h1>
-            <p className="text-3xl pb-[2%]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              tristique commodo nibh. In luctus suscipit mauris at dictum.
-              Suspendisse eget ex eleifend, tristique nisl vitae, hendrerit
-              lorem. Sed nec iaculis ante. Aliquam laoreet, mi eget facilisis
-              pellentesque, velit tortor sollicitudin neque, nec condimentum
-              risus orci a orci. Sed quis nisi elit. In hac habitasse platea
-              dictumst. Mauris rhoncus ligula vitae tortor faucibus auctor. Sed
-              feugiat sodales lobortis. Cras et urna quam.
-            </p>
-            <p className="text-3xl pb-[2%]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-              tristique commodo nibh. In luctus suscipit mauris at dictum.
-              Suspendisse eget ex eleifend, tristique nisl vitae, hendrerit
-              lorem. Sed nec iaculis ante. Aliquam laoreet, mi eget facilisis
-              pellentesque, velit tortor sollicitudin neque, nec condimentum
-              risus orci a orci. Sed quis nisi elit. In hac habitasse platea
-              dictumst. Mauris rhoncus ligula vitae tortor faucibus auctor. Sed
-              feugiat sodales lobortis. Cras et urna quam.
-            </p>
+          <div className="details-lighting flex-col w-full max-h-screen p-[5%] text-[#ffffff] overflow-y-scroll overflow-x-hidden scrollbar-hide">
+            <div className="flex flex-row">
+              <div className="flex items-center">
+                <a href={data.repo}>
+                  <img src={githubLogo} className="object-right w-[45%]" /></a>
+              </div>
+              <div className="flex justify-center w-full items-center">
+                <h1 className=" text-7xl pb-[2%] mr-[1%]">{data.name}</h1>
+                <a href={data.url} target="blank">
+                  <p className="flex-col text-6xl mb-[10%]">
+                    <span className="font-swissSymbol underline">b</span></p></a>
+              </div>
+
+            </div>
+            {data.description && data.description.map((p) => {
+              return (
+                <>
+                  <p className="text-3xl pb-[2%]">{p}</p>
+                </>
+              )
+            })}
           </div>
         </motion.div>
       </div>
