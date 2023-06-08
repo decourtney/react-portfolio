2f6642252061d84301a6707d63fb19994ff9ebf5
decourtney <donovan.courtney@gmail.com>
2023-06-08 10:45:38 -0700

    about page work

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import forrest from "../../images/forrest.jpg"
 
 const AboutLeft = () => {
   const [loadContent, setLoadContent] = useState("");
@@ -14,8 +15,19 @@ const AboutRight = () => {
   const [loadContent, setLoadContent] = useState("");
 
   return (
-    <div>
-      <p className="text-6xl">{ }</p>
+    <div className="w-full m-[5%]">
+      <div className="image-container relative w-full h-fit overflow-hidden bg-slate-600 text-4xl">
+        <img src={forrest} className="w-full invisible"/>
+        <div className="img absolute top-0 flex w-full h-full">
+          <span className="m-[1%] w-[20%] border overflow-hidden"></span>
+          <span className="m-[1%] w-[20%] border overflow-hidden"></span>
+          <span className="m-[1%] w-[20%] border overflow-hidden"></span>
+          <span className="m-[1%] w-[20%] border overflow-hidden"></span>
+          <span className="m-[1%] w-[20%] border overflow-hidden"></span>
+
+          {/* <img src={forrest} className="w-full "></img><img></img><img></img><img></img><img></img> */}
+        </div>
+      </div>
     </div>
   );
 };
