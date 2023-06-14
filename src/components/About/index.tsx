ca0bb6d8c1c9b920aef7a3c7ff324fe8a744a2d3
decourtney <donovan.courtney@gmail.com>
2023-06-14 12:06:35 -0700

    trivision board animates well - next need to control the animation

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,44 +4,48 @@ import forrest from "../../images/forrest1.png";
 
 const variants1 = {
   rotate: {
-    rotateY: [-60, 60, 180, 300],
-    transformPerspective: [1000, 1000, 2000, 1000],
-    x: ["-25%", "25%", "0%", "-25%"],
+    // rotateY: -60,
+    rotateY: [0, 120, 240, 360],
+    transformPerspective: [1000, 1000, 1000, 1000],
+    // x: ["-25%", "25%", "0%", "-25%"],
+    x: ["0%", "44%", "-44%", "0%"],
     transition: {
       repeat: Infinity,
-      duration: 3,
-      ease: "linear",
+      duration: 9,
+      // ease: "easeIn",
     },
   },
 };
 
 const variants2 = {
   rotate: {
-    rotateY: [-180, -60, 60, 180],
-    transformPerspective: [2000, 1000, 1000, 2000],
-    x: ["0%", "-25%", "25%", "0%"],
+    // rotateY: 240,
+    rotateY: [240, 360, 480, 600],
+    transformPerspective: [1000, 1000, 1000, 1000],
+    // x: ["0%", "-25%", "25%", "0%"],
+    x: ["-44%", "-0%", "44%", "0%"],
     transition: {
       repeat: Infinity,
-      duration: 3,
-      ease: "linear",
+      duration: 9,
+      // ease: "easeIn",
     },
   },
 };
 
 const variants3 = {
   rotate: {
-    rotateY: [60, 180, 300, 420],
-    transformPerspective: [1000, 2000, 1000, 1000],
-    x: ["25%", "0%", "-25%", "25%"],
+    // rotateY: 60,
+    rotateY: [120, 240, 360, 480],
+    transformPerspective: [1000, 1000, 1000, 1000],
+    // x: ["25%", "0%", "-25%", "25%"],
+    x: ["44%", "-44%", "0%", "44%"],
     transition: {
       repeat: Infinity,
-      duration: 3,
-      ease: "linear",
+      duration: 9,
+      // ease: "easeIn",
     },
   },
 };
-
-
 
 const AboutLeft = () => {
   const [loadContent, setLoadContent] = useState("");
@@ -60,110 +64,110 @@ const AboutRight = () => {
   return (
     <AnimatePresence>
       <div className="w-full m-[5%]">
-        <div className="image-container relative w-full h-fit  bg-slate-600 text-4xl">
+        <div className="image-container relative w-full h-fit   text-4xl">
           <img src={forrest} className="w-full invisible" />
           <div className="absolute top-0 left-0 flex w-[100%] h-full">
             <div className="img relative w-[20%] h-full">
               <motion.span
-              whileHover={{animation: "pause"}}
-                initial={{ rotateY: -60, x: "-25%" }}
+                initial={{ rotateY: 0, x: "0%" }}
                 variants={variants1}
                 animate="rotate"
-                className="image-1 absolute top-0 left-0 w-full h-full border"
+                className="image-1 absolute top-0 left-0 w-full h-full border z-20"
               />
               <motion.span
-                initial={{ rotateY: -180, x: "0%" }}
+                initial={{ rotateY: 240, x: "-44%" }}
                 variants={variants2}
                 animate="rotate"
-                className="image-2 absolute top-0 left-0 w-full h-full border"
+                className="image-2 absolute top-0 left-0 w-full h-full border z-10"
               />
               <motion.span
-                initial={{ rotateY: 60, x: "25%" }}
+                initial={{ rotateY: 120, x: "44%" }}
                 variants={variants3}
                 animate="rotate"
-                className="image-3 absolute top-0 left-0 w-full h-full border"
+                className="image-3 absolute top-0 left-0 w-full h-full border z-0"
               />
             </div>
             <div className="img relative w-[20%] h-full">
               <motion.span
-                initial={{ rotateY: -60, x: "-25%" }}
+                initial={{ rotateY: 0, x: "0%" }}
                 variants={variants1}
                 animate="rotate"
-                className="image-1 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-1 absolute top-0 left-0 w-full h-full border z-20"
               />
               <motion.span
-                initial={{ rotateY: -180, x: "0%" }}
+                initial={{ rotateY: 240, x: "-44%" }}
                 variants={variants2}
                 animate="rotate"
-                className="image-2 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-2 absolute top-0 left-0 w-full h-full border z-10"
               />
               <motion.span
-                initial={{ rotateY: 60, x: "25%" }}
+                initial={{ rotateY: 120, x: "44%" }}
                 variants={variants3}
                 animate="rotate"
-                className="image-3 absolute top-0 left-0 h-full w-[100%] border"
+                className="image-3 absolute top-0 left-0 w-full h-full border z-0"
               />
             </div>
             <div className="img relative w-[20%] h-full">
               <motion.span
-                initial={{ rotateY: -60, x: "-25%" }}
+                initial={{ rotateY: 0, x: "0%" }}
                 variants={variants1}
                 animate="rotate"
-                className="image-1 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-1 absolute top-0 left-0 w-full h-full border z-20"
               />
               <motion.span
-                initial={{ rotateY: -180, x: "0%" }}
+                initial={{ rotateY: 240, x: "-44%" }}
                 variants={variants2}
                 animate="rotate"
-                className="image-2 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-2 absolute top-0 left-0 w-full h-full border z-10"
               />
               <motion.span
-                initial={{ rotateY: 60, x: "25%" }}
+                initial={{ rotateY: 120, x: "44%" }}
                 variants={variants3}
                 animate="rotate"
-                className="image-3 absolute top-0 left-0 h-full w-[100%] border"
+                className="image-3 absolute top-0 left-0 w-full h-full border z-0"
               />
             </div>
             <div className="img relative w-[20%] h-full">
               <motion.span
-                initial={{ rotateY: -60, x: "-25%" }}
+                initial={{ rotateY: 0, x: "0%" }}
                 variants={variants1}
                 animate="rotate"
-                className="image-1 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-1 absolute top-0 left-0 w-full h-full border z-20"
               />
               <motion.span
-                initial={{ rotateY: -180, x: "0%" }}
+                initial={{ rotateY: 240, x: "-44%" }}
                 variants={variants2}
                 animate="rotate"
-                className="image-2 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-2 absolute top-0 left-0 w-full h-full border z-10"
               />
               <motion.span
-                initial={{ rotateY: 60, x: "25%" }}
+                initial={{ rotateY: 120, x: "44%" }}
                 variants={variants3}
                 animate="rotate"
-                className="image-3 absolute top-0 left-0 h-full w-[100%] border"
+                className="image-3 absolute top-0 left-0 w-full h-full border z-0"
               />
             </div>
             <div className="img relative w-[20%] h-full">
               <motion.span
-                initial={{ rotateY: -60, x: "-25%" }}
+                initial={{ rotateY: 0, x: "0%" }}
                 variants={variants1}
                 animate="rotate"
-                className="image-1 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-1 absolute top-0 left-0 w-full h-full border z-20"
               />
               <motion.span
-                initial={{ rotateY: -180, x: "0%" }}
+                initial={{ rotateY: 240, x: "-44%" }}
                 variants={variants2}
                 animate="rotate"
-                className="image-2 absolute top-0 left-0 h-[100%] w-[100%] border"
+                className="image-2 absolute top-0 left-0 w-full h-full border z-10"
               />
               <motion.span
-                initial={{ rotateY: 60, x: "25%" }}
+                initial={{ rotateY: 120, x: "44%" }}
                 variants={variants3}
                 animate="rotate"
-                className="image-3 absolute top-0 left-0 h-full w-[100%] border"
+                className="image-3 absolute top-0 left-0 w-full h-full border z-0"
               />
             </div>
+
           </div>
         </div>
       </div>
