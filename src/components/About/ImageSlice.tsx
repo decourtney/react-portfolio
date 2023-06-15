8129ac764a7a15d5fc6b76d258e91ca9bac0111d
decourtney <donovan.courtney@gmail.com>
2023-06-15 12:01:55 -0700

    changed hard coded numbers to variables in ImageSlice component

--- src/components/About/ImageSlice.tsx
+++ src/components/About/ImageSlice.tsx
@@ -1,66 +1,69 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-const variants1 = {
-  rotateY: [0, 120, 240, 360],
-  transformPerspective: [1000, 2000, 2000, 1000],
-  x: ["0%", "45%", "-45%", "0%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  },
-};
-
-const variants2 = {
-  rotateY: [240, 360, 480, 600],
-  transformPerspective: [2000, 1000, 2000, 1000],
-  x: ["-45%", "-0%", "45%", "0%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  },
-};
-
-const variants3 = {
-  rotateY: [120, 240, 360, 480],
-  transformPerspective: [2000, 2000, 1000, 2000],
-  x: ["45%", "-45%", "0%", "45%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  },
-};
-
 type ImageSliceProps = {
   i: number;
   s: number;
 };
 
 const ImageSlice = ({ i, s }: ImageSliceProps) => {
-  const pos = i * 10;
+  const pos = (100 / (s - 1)) * i;
+  const w = 100 / s;
+  const duration = 9;
+
+  const variants1 = {
+    rotateY: [0, 120, 240, 360],
+    transformPerspective: [1000, 2000, 2000, 1000],
+    x: ["0%", "45%", "-45%", "0%"],
+    transition: {
+      repeat: Infinity,
+      duration: duration,
+      ease: "easeInOut",
+    },
+  };
+
+  const variants2 = {
+    rotateY: [240, 360, 480, 600],
+    transformPerspective: [2000, 1000, 2000, 1000],
+    x: ["-45%", "-0%", "45%", "0%"],
+    transition: {
+      repeat: Infinity,
+      duration: duration,
+      ease: "easeInOut",
+    },
+  };
+
+  const variants3 = {
+    rotateY: [120, 240, 360, 480],
+    transformPerspective: [2000, 2000, 1000, 2000],
+    x: ["45%", "-45%", "0%", "45%"],
+    transition: {
+      repeat: Infinity,
+      duration: duration,
+      ease: "easeInOut",
+    },
+  };
+
+  
 
   return (
-    <div className={`img relative h-full`} style={{ width: `${100 / s}%` }}>
+    <div className={`img relative h-full`} style={{ width: `${w}%` }}>
       <motion.span
         initial={{ rotateY: 0, x: "0%" }}
         animate={variants1}
-        className={`image-1 absolute top-0 left-0 w-full h-full border`}
+        className={`image-1 absolute top-0 left-0 w-full h-full `}
         style={{ backgroundPosition: `${pos}%` }}
-        // This style is not working quite right
       />
       <motion.span
         initial={{ rotateY: 240, x: "-45%" }}
         animate={variants2}
-        className={`image-2 absolute top-0 left-0 w-full h-full bg-[${pos}%] border`}
+        className={`image-2 absolute top-0 left-0 w-full h-full `}
         style={{ backgroundPosition: `${pos}%` }}
       />
       <motion.span
         initial={{ rotateY: 120, x: "45%" }}
         animate={variants3}
-        className={`image-3 absolute top-0 left-0 w-full h-full bg-[${pos}%] border`}
+        className={`image-3 absolute top-0 left-0 w-full h-full `}
         style={{ backgroundPosition: `${pos}%` }}
       />
     </div>
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,56 +3,25 @@ import ImageSlice from "./ImageSlice";
 import { AnimatePresence, motion, useAnimate } from "framer-motion";
 import forrest from "../../images/forrest1.png";
 
-const variants1 = {
-  rotateY: [0, 120, 240, 360],
-  transformPerspective: [1000, 2000, 2000, 1000],
-  x: ["0%", "45%", "-45%", "0%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  },
-};
-
-const variants2 = {
-  rotateY: [240, 360, 480, 600],
-  transformPerspective: [2000, 1000, 2000, 1000],
-  x: ["-45%", "-0%", "45%", "0%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  },
-};
-
-const variants3 = {
-  rotateY: [120, 240, 360, 480],
-  transformPerspective: [2000, 2000, 1000, 2000],
-  x: ["45%", "-45%", "0%", "45%"],
-  transition: {
-    repeat: Infinity,
-    duration: 9,
-    // ease: "easeIn",
-  }
-};
-
 const AboutLeft = () => {
   const [loadContent, setLoadContent] = useState("");
 
   return (
     <div className="w-full mx-[3%] mt-[2%] mb-[5%] bg-black">
-      <p className="text-6xl">{ }</p>
+      <p className="text-6xl">{}</p>
     </div>
   );
 };
 
 const AboutRight = () => {
   const [loadContent, setLoadContent] = useState("");
-  const numberOfSlices = 5;
+  const numberOfSlices = 20;
   const slicesArray = [];
 
-  for(let i = 0; i < numberOfSlices; i++){
-    slicesArray.push(<ImageSlice i={i} s={numberOfSlices}/>)
+  for (let i = 0; i < numberOfSlices; i++) {
+    slicesArray.push(
+      <ImageSlice key={`image-slice-${i}`} i={i} s={numberOfSlices} />
+    );
   }
 
   return (
@@ -61,8 +30,9 @@ const AboutRight = () => {
         <div className="image-container relative w-full h-fit   text-4xl">
           <img src={forrest} className="w-full invisible" />
           <div className="absolute top-0 left-0 flex w-[100%] h-full">
-            {/* Split image slices into react component */}
-            {slicesArray.map((s)=> {return s})}
+            {slicesArray.map((s) => {
+              return s;
+            })}
           </div>
         </div>
       </div>
