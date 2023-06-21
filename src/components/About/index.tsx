acf1e43365ec5fcd1224d463702679d8da4ef4be
decourtney <53956789+decourtney@users.noreply.github.com>
2023-06-21 17:44:24 -0500

    template for about me content

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -33,8 +33,28 @@ const AboutRight = () => {
 
   return (
     <div className="flex justify-center items-center w-full ml-[4%] -z-10 bg-blue-400">
-      <div className="aboutme-container relative flex justify-center items-center w-full ml-[3%] mr-[6%]">
-        <img src={forrest} className="img w-full -z-10"/>
+      <div className="aboutme-container relative flex justify-center items-center w-[80%] h-[75%] shadow-md before:">
+        <span className="h-full">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          laoreet mi at mollis pretium. Integer ac est vulputate, malesuada elit
+          eu, lacinia erat. Maecenas volutpat fringilla elit at molestie. Morbi
+          ac justo eu neque tempus venenatis sit amet vitae nisl. Nunc eu
+          tristique arcu, sit amet gravida magna. Donec in lectus eget magna
+          placerat sodales malesuada a elit. Vivamus ultricies urna nec placerat
+          auctor. Vestibulum auctor vehicula maximus. Curabitur posuere gravida
+          nibh id varius. Aliquam sed venenatis odio, id ultricies ipsum.
+          Curabitur interdum gravida nisi, vel finibus massa finibus ut.
+          Phasellus eu lorem venenatis, fermentum dolor malesuada, ullamcorper
+          ante. Nunc sed porta lorem. In laoreet est et lacus placerat vehicula.
+          Praesent sollicitudin ullamcorper velit id lacinia. Cras in maximus
+          nisl, sed pharetra ligula. Pellentesque nunc dui, vestibulum tristique
+          purus sed, rhoncus vulputate neque. Vivamus id turpis risus. Ut sed
+          est tempus justo semper viverra quis ac mauris. Quisque id semper
+          diam, a consequat velit. Mauris fringilla erat mauris, consectetur
+          pellentesque urna sodales sit amet. Vivamus pulvinar enim quis congue
+          fermentum. Maecenas tristique elit eget risus sollicitudin, vitae
+          egestas est ornare.
+        </span>
       </div>
     </div>
   );
