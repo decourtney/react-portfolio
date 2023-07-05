c8018f8d130ee894087a2730fad42433fb85242e
decourtney <53956789+decourtney@users.noreply.github.com>
2023-07-05 18:20:36 -0500

    crt effect added and working on form style

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -43,43 +43,86 @@ const ContactRight = ({ data }: ContactProps) => {
   };
 
   return (
-    <div className="flex w-full ml-[4%] justify-center items-center bg-green-400">
-      <form
-        className="flex-col w-[60%] max-h-[60%] p-[1%] space-y-5 bg-pink-300"
-        onSubmit={sendEmail}
-      >
-        <input
-          type="text"
-          name="user_name"
-          className="w-full"
-          placeholder="Name"
-          required
-        />
-        <input
-          type="email"
-          name="user_email"
-          className="w-full"
-          placeholder="Email"
-          required
-        />
-        <div className="h-full">
-          <textarea
-            name="message"
-            className="w-full min-h-[100%] resize-none"
-            placeholder="Message"
-            required
-          />
+    <div className="flex w-full justify-center bg-green-400">
+      <div className="relative w-[80%] h-[60%] my-[5%] text-green-500 text-3xl">
+        <div className="video-overlay pointer-events-none" />
+        <div className="flex w-full h-full p-[2%] bg-black">
+          <form
+            className="flex-col w-full p-[1%] space-y-0 bg-gray-800"
+            onSubmit={sendEmail}
+          >
+            <div className="flex space-x-2">
+              <label htmlFor="username">Name:</label>
+              <input
+                id="username"
+                type="text"
+                name="user_name"
+                className="w-full bg-inherit"
+                placeholder=""
+                required
+              />
+            </div>
+            <div className="flex space-x-2">
+              <label htmlFor="email">Email:</label>
+              <input
+                id="email"
+                type="email"
+                name="user_email"
+                className="w-full bg-inherit"
+                placeholder=""
+                required
+              />
+            </div>
+            <div className="flex space-x-2  bg-blue-200">
+              <label htmlFor="message">Message:</label>
+              {/* <div className="w-full break-words h-full">TESTdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</div> */}
+              {/* <textarea
+                id="message"
+                name="message"
+                className="flex w-full h-full mt-1 scrollbar-hide"
+                placeholder=""
+                required
+              /> */}
+            </div>
+          </form>
         </div>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="submit"
-        >
-          Send
-          {/* <input type="submit" value="Send" /> */}
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
 
 export { ContactLeft, ContactRight };
+
+{
+  /* <form
+            className="flex-col h-full p-[1%] space-y-5 bg-orange-300"
+            onSubmit={sendEmail}
+          >
+            <input
+              type="text"
+              name="user_name"
+              className="w-full"
+              placeholder="Name"
+              required
+            />
+            <input
+              type="email"
+              name="user_email"
+              className="w-full"
+              placeholder="Email"
+              required
+            />
+            <textarea
+              name="message"
+              className="w-full resize-none"
+              placeholder="Message"
+              required
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Send
+            </button>
+          </form> */
+}
