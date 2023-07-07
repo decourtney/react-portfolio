927bb7ff38c31aef273c17a2a47768100d405ae8
decourtney <donovan.courtney@gmail.com>
2023-07-07 10:49:22 -0700

    contact message display working

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -45,45 +45,47 @@ const ContactRight = ({ data }: ContactProps) => {
 
   return (
     <div className="flex w-full justify-center bg-green-400">
-      <div className="relative flex-col w-[80%] h-[60%] mt-[10%] text-3xl font-vt323 bg-gray-800">
-        {/* <div className="video-overlay pointer-events-none" /> */}
+      <div className="relative flex-col w-[80%] h-[60%] mt-[10%] text-3xl font-vt323 text-green-500 bg-gray-800">
+        <div className="video-overlay pointer-events-none" />
         <form
-          className="inline-flex flex-col w-full h-full space-y-0"
+          className="inline-flex flex-col w-full h-full p-[2%] space-y-0"
           onSubmit={sendEmail}
         >
-          <div className="flex">
-            <label htmlFor="username">Name:</label>
+          <div className="flex ">
+            {/* <label htmlFor="username">Name:</label> */}
             <input
               id="username"
               type="text"
               name="user_name"
-              className="w-full bg-inherit outline-none"
-              style={{ fontVariant: "normal" }}
-              placeholder="Name"
+              className="w-full bg-inherit outline-none placeholder-green-400"
+              title="Name"
+              placeholder="<Name>"
               required
+              autoFocus
             />
           </div>
-          <div className="flex bg-pink-400">
-            <label htmlFor="email">Email:</label>
+          <div className="flex text-inherit">
+            {/* <label htmlFor="email">Email:</label> */}
             <input
               id="email"
               type="email"
               name="user_email"
-              className="w-full bg-inherit outline-none"
-              placeholder="Email"
+              className="w-full bg-inherit outline-none placeholder-green-400"
+              title="Email"
+              placeholder="<Email>"
               required
             />
           </div>
-          <div className="flex h-full bg-blue-600">
-            <label htmlFor="message">Message:</label>
-            <div><span></span></div>
-            {/* <textarea
+          <div className="flex w-full h-full text-inherit">
+            {/* <label htmlFor="message">Message:</label> */}
+            <textarea
               id="message"
               name="message"
-              className="h-full w-full bg-inherit resize-none outline-none scrollbar-hide"
-              placeholder="Message"
+              className="h-full w-full bg-inherit resize-none outline-none placeholder-green-400 scrollbar-hide"
+              title="Message Contents"
+              placeholder="<Message>"
               required
-            /> */}
+            />
           </div>
         </form>
       </div>
