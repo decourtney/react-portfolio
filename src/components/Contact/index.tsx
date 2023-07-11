f8f22e0aef8866cc473186b9d81728c3fa6b64cd
decourtney <donovan.courtney@gmail.com>
2023-07-11 08:09:01 -0700

    positioning contact terminal - not complete

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -44,51 +44,55 @@ const ContactRight = ({ data }: ContactProps) => {
   return (
     <div className="flex w-full justify-center ml-[4%] bg-green-400">
       <div className="relative flex-col w-[80%] h-[60%] mt-[10%] rounded-lg text-3xl font-vt323 text-green-500 bg-gray-800">
-        <img src={tFrame} className="absolute top-0 left-0 w-full h-full" />
-        <div className="relative m-10 w-full h-full">
-          <motion.div
-            className="video-overlay absolute top-0 left-0 w-full h-full after:absolute after:w-full after:h-full pointer-events-none"
-            animate={{ translateY: "1px" }}
-            transition={{ duration: 0.1, repeat: Infinity, ease: "linear" }}
-          />
-          <form
-            className="inline-flex flex-col w-full h-full p-[2%] space-y-0"
-            onSubmit={sendEmail}
-          >
-            <div className="flex ">
-              <input
-                id="username"
-                type="text"
-                name="user_name"
-                className="w-full bg-inherit outline-none placeholder-green-400"
-                title="Name"
-                placeholder="<Name>"
-                required
-              />
-            </div>
-            <div className="flex text-inherit">
-              <input
-                id="email"
-                type="email"
-                name="user_email"
-                className="w-full bg-inherit outline-none placeholder-green-400"
-                title="Email"
-                placeholder="<Email>"
-                required
-              />
-            </div>
-            <div className="flex w-full h-full text-inherit">
-              <textarea
-                id="message"
-                name="message"
-                className="h-full w-full bg-inherit resize-none outline-none placeholder-green-400 scrollbar-hide"
-                title="Message Contents"
-                placeholder="<Message>"
-                required
-              />
-            </div>
-          </form>
-        </div>
+        {/* <img
+          src={tFrame}
+          className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"
+        /> */}
+        <div className="terminal-frame absolute top-0 left-0 h-full w-full z-20" />
+
+        <motion.div
+          className="video-overlay absolute top-1/2 left-1/2 w-[95%] h-[95%] rounded-xl after:rounded-xl after:absolute after:w-full after:h-full pointer-events-none"
+          initial={{ translateX: "-50%", translateY: "-50%" }}
+          animate={{ translateY: "-49%" }}
+          transition={{ duration: 0.1, repeat: Infinity, ease: "linear" }}
+        />
+        <form
+          className="inline-flex flex-col w-full h-full p-[2%] space-y-0"
+          onSubmit={sendEmail}
+        >
+          <div className="flex text-inherit">
+            <input
+              id="username"
+              type="text"
+              name="user_name"
+              className="w-full bg-inherit outline-none placeholder-green-400"
+              title="Name"
+              placeholder="<Name>"
+              required
+            />
+          </div>
+          <div className="flex text-inherit">
+            <input
+              id="email"
+              type="email"
+              name="user_email"
+              className="w-full bg-inherit outline-none placeholder-green-400"
+              title="Email"
+              placeholder="<Email>"
+              required
+            />
+          </div>
+          <div className="flex w-full h-full text-inherit">
+            <textarea
+              id="message"
+              name="message"
+              className="h-full w-full bg-inherit resize-none outline-none placeholder-green-400 scrollbar-hide"
+              title="Message Contents"
+              placeholder="<Message>"
+              required
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
