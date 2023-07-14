1276b8f6ceb2015f5a913aa26250443d070577c5
decourtney <53956789+decourtney@users.noreply.github.com>
2023-07-13 21:46:21 -0500

    added email form to emailform comp

--- src/components/Contact/EmailForm.tsx
+++ src/components/Contact/EmailForm.tsx
@@ -2,44 +2,42 @@ import React, { useEffect, useState, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 const TerminalEmailForm = () => {
-  return <div>Email Form</div>;
+  return (
+    <form className="w-full h-full">
+      <div className="flex text-inherit">
+        <input
+          id="username"
+          type="text"
+          name="user_name"
+          className="w-full bg-inherit outline-none placeholder-green-400"
+          title="Name"
+          placeholder="<Name>"
+          required
+        />
+      </div>
+      <div className="flex text-inherit">
+        <input
+          id="email"
+          type="email"
+          name="user_email"
+          className="w-full bg-inherit outline-none placeholder-green-400"
+          title="Email"
+          placeholder="<Email>"
+          required
+        />
+      </div>
+      <div className="flex w-full h-full text-inherit">
+        <textarea
+          id="message"
+          name="message"
+          className="h-full w-full bg-inherit resize-none outline-none placeholder-green-400 scrollbar-hide"
+          title="Message Contents"
+          placeholder="<Message>"
+          required
+        />
+      </div>
+    </form>
+  );
 };
 
 export default TerminalEmailForm;
-
-{
-  /* <form className="absolute top-1/2 left-1/2 w-[53%] h-[57%] p-8 -translate-x-[50%] -translate-y-[66%] text-xl font-vt323 text-green-500 bg-gray-800">
-          <div className="flex text-inherit">
-            <input
-              id="username"
-              type="text"
-              name="user_name"
-              className="w-full bg-inherit outline-none placeholder-green-400"
-              title="Name"
-              placeholder="<Name>"
-              required
-            />
-          </div>
-          <div className="flex text-inherit">
-            <input
-              id="email"
-              type="email"
-              name="user_email"
-              className="w-full bg-inherit outline-none placeholder-green-400"
-              title="Email"
-              placeholder="<Email>"
-              required
-            />
-          </div>
-          <div className="flex w-full h-full text-inherit">
-            <textarea
-              id="message"
-              name="message"
-              className="h-full w-full bg-inherit resize-none outline-none placeholder-green-400 scrollbar-hide"
-              title="Message Contents"
-              placeholder="<Message>"
-              required
-            />
-          </div>
-        </form> */
-}
