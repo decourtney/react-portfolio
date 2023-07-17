38b05e311e78b1e8e070c91fa3b8a96149b8d060
decourtney <donovan.courtney@gmail.com>
2023-07-17 09:32:52 -0700

    title string being split and animated correctly

--- src/components/Contact/MainMenu.tsx
+++ src/components/Contact/MainMenu.tsx
@@ -3,33 +3,44 @@ import { AnimatePresence, motion } from "framer-motion";
 
 const wordAnim = {
   visible: {},
-  hidden: { transitionEnd: { display: "none" } },
+  hidden: {},
 };
 
 const characterAnim = {
-  visible: { opacity: 1 },
-  hidden: { opacity: 0, transition: { duration: 1 } },
+  // visible: { opacity: 1 },
+  // hidden: { opacity: 0, transition: { delay: 1, duration: 0.5 }, },
+  visible: { display: 'inline' },
+  hidden: { display: 'none', transition: { delay: 1, duration: 1 }, },
 };
 
 const TerminalMainMenu = () => {
   const text = "Framework for User Engagement";
+
   return (
     <div className="flex justify-center text-4xl">
-      {text.split(" ").map((word, index) => (
+      {text.split(" ").map(([fChar, ...rChars], index) => (
         <motion.span
           key={index}
           initial="visible"
           animate="hidden"
           variants={wordAnim}
-          transition={{delay: 1, delayChildren: index * 0.25, staggerChildren: 0.5 }}
+          transition={{ delay: 1, delayChildren: index * 0.25, staggerChildren: 0.5, }} //stagger for individual chars
         >
-          {word.split("").map((character, index) => {
-            return (
-              <motion.span key={index} variants={characterAnim}>
-                {character}
-              </motion.span>
-            );
-          })}
+          <span className="text-blue-500">{fChar}</span>
+          <motion.span key={index} variants={characterAnim}>
+            {rChars}
+          </motion.span>
+          {/* {
+            rChars.map((character, index) => {
+              return (
+                <>
+                  <motion.span key={index} variants={characterAnim}>
+                    {character}
+                  </motion.span>
+                </>
+              );
+            })
+          } */}
         </motion.span>
       ))}
     </div>
