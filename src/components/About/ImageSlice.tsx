a71f310b02846af2e37f4cc3e17282fc8330b1c5
decourtney <53956789+decourtney@users.noreply.github.com>
2023-08-02 20:03:26 -0500

    dropped Lenticular component - pulling the reins

--- src/components/About/ImageSlice.tsx
+++ src/components/About/ImageSlice.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { motion, useAnimate } from "framer-motion";
+ import shadowbox_rod from "../../images/shadowbox_rod.png"
 
 type ImageSliceProps = {
   sliceWidth: number;
@@ -37,29 +38,34 @@ const ImageSlice = ({ sliceWidth, slicePos, delay, duration }: ImageSliceProps) 
   // Replace images with:
   // Passion, Resilience, Creativity
   return (
-    <div className={`img relative h-full`} style={{ width: `${sliceWidth}%` }}>
-      <motion.span
-        className={`image-1 absolute top-0 left-0 w-full h-full will-change-transform`}
-        style={{ backgroundPosition: `${slicePos}%` }}
-        initial={{ rotateY: 0, x: "0%" }}
-        animate={variants1}
-        transition={transition}
-      />
-      <motion.span
-        className={`image-2 absolute top-0 left-0 w-full h-full will-change-transform`}
-        style={{ backgroundPosition: `${slicePos}%` }}
-        initial={{ rotateY: 240, x: "-45%" }}
-        animate={variants2}
-        transition={transition}
-      />
-      <motion.span
-        className={`image-3 absolute top-0 left-0 w-full h-full will-change-transform`}
-        style={{ backgroundPosition: `${slicePos}%` }}
-        initial={{ rotateY: 120, x: "45%" }}
-        animate={variants3}
-        transition={transition}
-      />
-    </div>
+    <>
+      <div
+        className={`img relative h-full`}
+        style={{ width: `${sliceWidth}%` }}
+      >
+        <motion.span
+          className={`image-1 absolute top-0 left-0 w-full h-full will-change-transform`}
+          style={{ backgroundPosition: `${slicePos}%` }}
+          initial={{ rotateY: 0, x: "0%" }}
+          animate={variants1}
+          transition={transition}
+        />
+        <motion.span
+          className={`image-2 absolute top-0 left-0 w-full h-full will-change-transform`}
+          style={{ backgroundPosition: `${slicePos}%` }}
+          initial={{ rotateY: 240, x: "-45%" }}
+          animate={variants2}
+          transition={transition}
+        />
+        <motion.span
+          className={`image-3 absolute top-0 left-0 w-full h-full will-change-transform`}
+          style={{ backgroundPosition: `${slicePos}%` }}
+          initial={{ rotateY: 120, x: "45%" }}
+          animate={variants3}
+          transition={transition}
+        />
+      </div>
+    </>
   );
 };
 
--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useRef } from "react";
 import ImageSlice from "./ImageSlice";
-import Lenticular from "./Lenticular";
-import LenticularToo from "./LenticularToo";
 import { AnimatePresence, motion, useAnimate, useScroll } from "framer-motion";
 import forrest from "../../images/forrest1.png";
 import lilith from "../../images/lilith.png";
 import biden from "../../images/biden.png";
+import shadowbox_frame from "../../images/shadowbox_frame.png";
+import shadowbox_rod from "../../images/shadowbox_rod.png"
 
 const AboutLeft = () => {
   const numberOfSlices = 15;
@@ -36,7 +36,7 @@ const AboutLeft = () => {
   };
 
   return (
-    <div className="flex justify-center items-center w-full mr-[4%] pl-[4%] pr-[2%] -z-10 bg-blue-400">
+    <div className="flex justify-center items-center w-full mr-[4%] pl-[4%] pr-[2%] -z-10">
       <div className="trivision-board relative w-full h-fit ">
         <img src={forrest} className="w-full invisible" />
         <div className="absolute top-0 left-0 flex w-[100%] h-full space-x-0">
@@ -114,99 +114,119 @@ const AboutRight = () => {
   }, [windowSize]);
 
   return (
-    <div
-      id="info-box"
-      className="flex justify-center items-center w-full ml-[4%] bg-blue-400"
-      onMouseLeave={() => setMousePos({ x: middlePos.x, y: middlePos.y })} // Reset box if not mouse over panel
-    >
-      <motion.div
-        className="relative flex-col justify-center items-center w-[80%] h-[75%] shadow-2xl will-change-transform"
-        style={{ transformStyle: "preserve-3d" }}
-        animate={{ rotateX: -1 * offset.y + "deg", rotateY: offset.x + "deg" }}
-        transition={{ type: "tween", ease: "easeOut" }}
+    <>
+      <div
+        id="info-box"
+        className="flex justify-center items-center w-full h-full ml-[4%] mr-[2%]"
+        onMouseLeave={() => setMousePos({ x: middlePos.x, y: middlePos.y })} // Reset box if not mouse over panel
       >
-        <div className=" w-full h-full  scrollbar-hide">
-          <LenticularToo key={"lenticular"} />
-          
-          {/* <div className="w-full p-5 text-2xl space-y-3">
-            <p>
-              As a dedicated web developer, my coding journey spans several
-              years, with a formal focus on web development beginning in 2022.
-              Throughout my experience, I've worked with the MERN stack and
-              MySQL, building full-stack web applications that showcase my
-              abilities in both front-end and back-end development.
-            </p>
-            <p>
-              Adaptability is a core strength, and I'm always eager to explore
-              new frameworks and technologies. Keeping my programming skills
-              fresh through side projects, I'm well-prepared to tackle diverse
-              challenges in the ever-evolving field of web development.
-            </p>
-            <p>
-              Beyond coding, you can find me sketching designs and drawing
-              inspiration from various sources. My passion for creativity and
-              problem-solving drives me to create exceptional digital
-              experiences that leave a lasting impact. I'm thrilled to
-              collaborate with like-minded professionals and embark on new
-              ventures, continuously learning and growing in this dynamic
-              industry. Let's bring innovative web applications to life
-              together!
-            </p>
-          </div> */}
+        <div className="absolute flex top-0 left-1/2 w-full h-full">
+          <img src={shadowbox_rod} />
         </div>
-        <div
-          id="box-top"
-          className="absolute top-0 left-0 w-full h-full pointer-events-none"
-          style={{ transformStyle: "preserve-3d" }}
+
+        <motion.div
+          className="relative flex-col justify-center items-center w-[90%] h-[80%] will-change-transform bg-slate-600"
+          style={{
+            transformStyle: "preserve-3d",
+            boxShadow: "0px 0px 10px 0px rgb(23 23 23)",
+          }}
+          animate={{
+            // rotateX: -1 * offset.y + "deg", // Not needed right now
+            rotateY: offset.x + "deg",
+          }}
+          transition={{ type: "tween", ease: "easeOut" }}
         >
-          {/* Container for the glass glare effect */}
-          <div
-            id="glass"
-            className="relative w-full h-full overflow-clip backdrop-blur-[1px]"
-            style={{
-              transform: "translateZ(50px)",
-              backgroundImage:
-                "radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 0) 65%, rgba(103, 74, 49, 0.5) 84%, rgba(43, 23, 0, 0.81) 100%)",
-            }}
-          >
-            <motion.div
-              id="glass-glare"
-              className="absolute top-1/2 left-1/2 w-[600%] h-[600%] opacity-20 blur-sm pointer-events-none will-change-transform"
-              style={{
-                background:
-                  "linear-gradient(0.4turn, #00000000 58%, #ffffff, #00000000 60%)",
-              }}
-              initial={{ translateX: "-50%", translateY: "-50%" }}
-              animate={{
-                translateX: offset.x + -50 + "%",
-                translateY: offset.y + -50 + "%",
-              }}
-              transition={{ type: "tween", ease: "easeOut" }}
-            />
+          <div className=" w-full h-full overflow-scroll scrollbar-hide">
+            <div className="w-full p-5 text-2xl space-y-3">
+              <p>
+                As a dedicated web developer, my coding journey spans several
+                years, with a formal focus on web development beginning in 2022.
+                Throughout my experience, I've worked with the MERN stack and
+                MySQL, building full-stack web applications that showcase my
+                abilities in both front-end and back-end development.
+              </p>
+              <p>
+                Adaptability is a core strength, and I'm always eager to explore
+                new frameworks and technologies. Keeping my programming skills
+                fresh through side projects, I'm well-prepared to tackle diverse
+                challenges in the ever-evolving field of web development.
+              </p>
+              <p>
+                Beyond coding, you can find me sketching designs and drawing
+                inspiration from various sources. My passion for creativity and
+                problem-solving drives me to create exceptional digital
+                experiences that leave a lasting impact. I'm thrilled to
+                collaborate with like-minded professionals and embark on new
+                ventures, continuously learning and growing in this dynamic
+                industry. Let's bring innovative web applications to life
+                together!
+              </p>
+            </div>
           </div>
           <div
-            id="left-wall"
-            className="absolute top-0 left-0 w-[50px] h-full origin-left bg-slate-500"
-            style={{ transform: "rotateY(-90deg)" }}
-          />
-          <div
-            id="right-wall"
-            className="absolute top-0 right-0 w-[50px] h-full origin-right bg-slate-500"
-            style={{ transform: "rotateY(90deg)" }}
-          />
-          <div
-            id="top-wall"
-            className="absolute top-0 right-0 w-full h-[50px] origin-top shadow-lg bg-slate-500"
-            style={{ transform: "rotateX(90deg)" }}
-          />
-          <div
-            id="bottom-wall"
-            className="absolute bottom-0 left-0 w-full h-[50px] origin-bottom bg-slate-500"
-            style={{ transform: "rotateX(-90deg)" }}
-          />
-        </div>
-      </motion.div>
-    </div>
+            id="box-top"
+            className="absolute top-0 left-0 w-full h-full pointer-events-none"
+            style={{ transformStyle: "preserve-3d" }}
+          >
+            <div
+              className="absolute w-full h-full z-10"
+              style={{
+                translate: "0 0 40px",
+                borderImageSource: `url(${shadowbox_frame})`,
+                borderImageWidth: "100%",
+                borderImageSlice: "50%",
+                boxShadow: "0px 0px 30px 10px inset",
+              }}
+            />
+            {/* Container for the glass glare effect */}
+            <div
+              id="glass"
+              className="relative w-full h-full overflow-clip backdrop-blur-[1px]"
+              style={{
+                transform: "translateZ(40px)",
+                backgroundImage:
+                  "radial-gradient(ellipse at 50% 50%, rgba(255, 255, 255, 0) 65%, rgba(103, 74, 49, 0.5) 84%, rgba(43, 23, 0, 0.81) 100%)",
+              }}
+            >
+              <motion.div
+                id="glass-glare"
+                className="absolute top-1/2 left-1/2 w-[600%] h-[600%] opacity-20 blur-sm pointer-events-none will-change-transform"
+                style={{
+                  background:
+                    "linear-gradient(0.4turn, #00000000 58%, #ffffff, #00000000 60%)",
+                }}
+                initial={{ translateX: "-50%", translateY: "-50%" }}
+                animate={{
+                  translateX: offset.x + -50 + "%",
+                  translateY: offset.y + -50 + "%",
+                }}
+                transition={{ type: "tween", ease: "easeOut" }}
+              />
+            </div>
+            <div
+              id="left-wall"
+              className="absolute top-0 left-0 w-[40px] h-full origin-left bg-slate-800"
+              style={{ transform: "rotateY(-90deg)" }}
+            />
+            <div
+              id="right-wall"
+              className="absolute top-0 right-0 w-[40px] h-full origin-right bg-slate-800"
+              style={{ transform: "rotateY(90deg)" }}
+            />
+            <div
+              id="top-wall"
+              className="absolute top-0 right-0 w-full h-[40px] origin-top shadow-lg bg-slate-800"
+              style={{ transform: "rotateX(90deg)" }}
+            />
+            <div
+              id="bottom-wall"
+              className="absolute bottom-0 left-0 w-full h-[40px] origin-bottom bg-slate-800"
+              style={{ transform: "rotateX(-90deg)" }}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </>
   );
 };
 
