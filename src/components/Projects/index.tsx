6e98405eed29d8525f9217e4c51d2f898e69ad9a
decourtney <53956789+decourtney@users.noreply.github.com>
2023-08-22 20:53:00 -0500

    projects page almost done - reskin and adjust flickering lights

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -11,7 +11,10 @@ import {
 } from "framer-motion";
 import ProjectDetails from "./ProjectDetails";
 import namePlateBorder from "../../images/proj_nameplate_border.png";
-import namePlateBG from "../../images/proj_nameplate_bg.png"
+import namePlateHead from "../../images/proj_nameplate_head.png";
+import namePlateTail from "../../images/proj_nameplate_tail.png";
+import namePlateButtonFrame from "../../images/proj_nameplate_button_frame.png";
+import namePlateButton from "../../images/proj_nameplate_button.png";
 
 interface Project {
   name: string;
@@ -26,7 +29,7 @@ interface Project {
 const carouselVariants = {
   enter: (direction: number) => ({
     y: direction > 0 ? "51%" : "-51%",
-    opacity: 1,
+    opacity: 0,
     scale: 0.7,
     rotateX: direction > 0 ? -90 : 90,
     transformPerspective: 5000,
@@ -45,10 +48,11 @@ const carouselVariants = {
       duration: 1,
       ease: "easeInOut",
     },
+    zIndex: 1,
   },
   exit: (direction: number) => ({
     y: direction > 0 ? "-51%" : "51%",
-    opacity: 1,
+    opacity: 0,
     scale: 0.7,
     rotateX: direction > 0 ? 90 : -90,
     transformPerspective: 5000,
@@ -71,7 +75,7 @@ const ProjectLeft = ({ data }: { data: Project[] }) => {
   return (
     <>
       {/* Carousel repurposed from https://dev.to/satel/animated-carousel-with-framer-motion-2fp */}
-      <div className="relative w-full mx-[3%] my-[4%] -translate-x-[1%] -translate-y-[2%] -z-10 bg-black">
+      <div className="relative w-full mx-[3%] my-[4%] -translate-x-[1%] -translate-y-[2%] bg-black">
         <AnimatePresence custom={direction}>
           <motion.div
             key={currentPage}
@@ -83,7 +87,7 @@ const ProjectLeft = ({ data }: { data: Project[] }) => {
             exit="exit"
             custom={direction}
           >
-            <div className="project-image-border border-ws absolute top-0 left-0 w-full h-full"></div>
+            <div className="project-image-border border-ws absolute top-0 left-0 w-full h-full" />
             <img
               className="h-full w-full px-[4%] py-[3%]"
               src={
@@ -138,35 +142,42 @@ const ProjectRight = ({ data }: { data: Project[] }) => {
   };
 
   return (
-    <div className="flex w-full mx-[3%] mt-[2%] mb-[5%] overflow-auto scrollbar-hide bg-black">
-      <div className=" w-full h-full mt-[5%] ml-[2%] bg-[#aaaa8c]">
-        <img src={namePlateBG} className="w-full" />
-        <ul className="flex-col columns-2 w-full list-none space-y-4 bg-[#aaaa8c]">
+    <div className="panel-bg border-ws flex justify-center items-center w-full mx-[3%] mt-[2%] mb-[5%] overflow-auto scrollbar-hide ">
+      <div className="w-full px-[10%]">
+        <img src={namePlateHead} className="w-full" />
+        <ul className="project-nameplate-bg border-ws flex-col columns-2 space-y-[5%] py-[3%] list-none ">
           {data.map((project, index) => {
             return (
               <>
-                <div className="relative h-fit">
-                  <div className="project-nameplate-border border-ws pointer-events-none">
-                    <img src={namePlateBorder} className="w-[75%] invisible" />
-                  </div>
-                  <li
-                    key={project.name}
-                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full cursor-pointer"
-                  >
-                    <p
-                      className="text-[2.4vw] text-center text-black"
-                      onMouseEnter={() => handleMouseEnter(index)}
-                      onMouseLeave={handleMouseLeave}
+                <li
+                  key={project.name}
+                  className="relative flex space-x-[2%] mx-[5%]"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                  onMouseLeave={handleMouseLeave}
+                >
+                  <div className="relative w-[30%]">
+                    <img src={namePlateButtonFrame} className="w-[100%]" />
+                    <img
+                      src={namePlateButton}
+                      className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2 cursor-pointer active:scale-95"
                       onClick={() => handleDetailsDisplay(true, index)}
-                    >
-                      <span>{project.name}</span>
-                    </p>
-                  </li>
-                </div>
+                    />
+                  </div>
+
+                  <div className="project-nameplate-border border-ws relative w-full">
+                    <img src={namePlateBorder} className="w-[75%] invisible" />
+                    <div className="absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2">
+                      <p className="text-[1.2vw] text-center tracking-tight leading-[80%] text-black">
+                        <span>{project.name}</span>
+                      </p>
+                    </div>
+                  </div>
+                </li>
               </>
             );
           })}
         </ul>
+        <img src={namePlateTail} className="w-full" />
         <AnimatePresence mode="wait">
           {isDetails.display && (
             <ProjectDetails
