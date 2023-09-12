28ea77a63eb22e303a8e435d62a886808cd7b27e
decourtney <53956789+decourtney@users.noreply.github.com>
2023-09-12 13:56:04 -0500

    changed marquee frame image to png

--- src/pages/MobileTemp.tsx
+++ src/pages/MobileTemp.tsx
@@ -1,11 +1,11 @@
 // This will be the temporary mobile page until I can create/rewrite for mobile devices
-import construction_site from "../images/construction_site.jpg"
+import construction_site from "../images/mobile_construction_site.jpg"
 
 const MobileHome = () => {
   return (
     <main className="w-full h-[100vh] bg-[#00afff]">
       <section className="flex w-full h-full">
-        <img src={construction_site} className="w-full blur-[2px]"/>
+        <img src={construction_site} className="w-full blur-[2px]" />
         <div className="mobile-construction absolute w-full h-full" />
       </section>
     </main>
